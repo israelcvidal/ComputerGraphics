Fixing u vector in quaternion

transformation/transformation.py
@@ -130,7 +130,7 @@ def get_quaternion_matrix(p1, p2, theta):
 
     # (2)
     # unit rotation axis
-    u = p2-p1/np.linalg.norm(p2-p1)
+    u = (p2-p1)/np.linalg.norm(p2-p1)
 
     # (3)
     q = np.array(list((math.sin(half_theta) * u)) + [math.cos(half_theta)])
@@ -155,7 +155,6 @@ def get_quaternion_matrix(p1, p2, theta):
                     [q_[y], -q_[x], q_[w], q_[z]],
                     [-q_[x], -q_[y], -q_[z], q_[w]]]
                     )
-
     return compose_matrices([inverse_translation_matrix, L_q, R_q_, translation_matrix])
 
 
@@ -222,12 +221,25 @@ def get_axis(axis):
 
 
 if __name__ == '__main__':
-    p1 = [0, 1, 0]
+    p1 = np.array([0, 1, 0])
 
-    p2 = [0, 2, 0]
+    p2 = np.array([0, 20, 0])
 
-    print(get_arbitrary_rotation_matrix(p1, p2, 30))
-    print(get_quaternion_matrix(p1, p2, 30))
+    p = np.array([4, 5, 6, 0])
+
+    RM = get_arbitrary_rotation_matrix(p1, p2, 50)
+    QM = get_quaternion_matrix(p1, p2, 50)
+    R = get_rotation_matrix(axis='y', theta=50)
+    # print(R)
+    # print(RM)
+    # print(QM)
+
+    # for M in [RM, QM, R]:
+    #     print(M.dot(p))
+    # print('RM: ', RM)
+    # print('QM: ', QM)
+    # print('RMP: ', np.dot(RM, p))
+    # print('QMP: ', np.dot(QM, p))
     # print(get_rotation_matrix(theta=30, axis='y'))
 
     # print("arbitraty rotation matrix = ", get_arbitrary_rotation_matrix(p1=[0, 1, 0],  p2=[0, 2, 0], theta=30))