fe32e2e5f1c1a7a3372de923273b300992831534
israelcvidal <israelcvidal@gmail.com>
2017-10-11 22:06:31 -0300

    changing variable names

--- test/ap1.py
+++ test/ap1.py
@@ -10,26 +10,26 @@ def prova():
 
     scale_matrix = get_scale_matrix(np.array([5*math.sqrt(2)/4, 5*math.sqrt(2), 5*math.sqrt(2)/11, 1]))
     print("scale matrix:\n", scale_matrix)
-    _p1 = scale_matrix.dot(p1)
-    _p2 = scale_matrix.dot(p2)
-    _p3 = scale_matrix.dot(p3)
-    _p4 = scale_matrix.dot(p4)
+    p1 = scale_matrix.dot(p1)
+    p2 = scale_matrix.dot(p2)
+    p3 = scale_matrix.dot(p3)
+    p4 = scale_matrix.dot(p4)
 
     print("\nscaled points:")
 
-    for p in [_p2, _p3, _p4]:
+    for p in [p2, p3, p4]:
         print(p)
 
     print("\ntranslation _p3->origin:")
-    translation_p3_o = get_translation_matrix(-_p3[:3])
+    translation_p3_o = get_translation_matrix(-p3[:3])
     print(translation_p3_o)
 
-    N = np.cross((_p3-_p2)[0:3], (_p4-_p2)[:3])
+    N = np.cross((p3-p2)[0:3], (p4-p2)[:3])
     n = N/np.linalg.norm(N)
     n_after = np.array([0, 0, -1])
-    theta = np.degrees(math.acos(n.dot(n_after)))
+    theta = np.rad2deg(math.acos(n.dot(n_after)))
 
-    quaternion_matrix = get_quaternion_matrix(_p3[:3], _p4[:3], theta)
+    quaternion_matrix = get_quaternion_matrix(p3[:3], p4[:3], theta)
     print("\nquaternion matrix - angle " + str(theta))
     print(quaternion_matrix)
 
@@ -40,7 +40,7 @@ def prova():
     u_o_p3_ = p3_[:3]/np.linalg.norm(p3_[:3])
 
     # angulo entre os dois vetores unitários:
-    alpha = np.degrees(math.acos(u_o_p3_.dot(u_o_p2)))
+    alpha = np.rad2deg(math.acos(u_o_p3_.dot(u_o_p2)))
 
     rotation_z = get_rotation_matrix(-alpha, "z")
     print("\nrotation z:")
@@ -51,7 +51,7 @@ def prova():
     matrix = compose_matrices([translation_o_p3,  rotation_z, quaternion_matrix, translation_p3_o])
 
     print("\npoints after transformation:")
-    points = [_p1, _p2, _p3, _p4]
+    points = [p1, p2, p3, p4]
     for i, p in enumerate(points):
         points[i] = matrix.dot(p)
         print("p" + str(i+1) + ": ")
