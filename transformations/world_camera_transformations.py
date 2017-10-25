45a748b4b182426bd32d9cee2715230debb1a87e
israelcvidal <israelcvidal@gmail.com>
2017-10-25 19:34:03 -0300

    Creating function to main

--- transformations/world_camera_transformations.py
+++ transformations/world_camera_transformations.py
@@ -1,35 +1,38 @@
 import numpy as np
 
+
 def get_world_camera_matrix(po, look_at, avup):
     ic, jc, kc = get_ijk(po, look_at, avup)
 
-    #Appending the last column for the W->C Matrix
+    # Appending the last column for the W->C Matrix
     i_wc = np.append(ic, -np.dot(ic, po))
     j_wc = np.append(jc, -np.dot(jc, po))
     k_wc = np.append(kc, -np.dot(kc, po))
     last_row = np.array([0, 0, 0, 1])
     
-    #Stacking arrays as rows for the W->C Matrix
+    # Stacking arrays as rows for the W->C Matrix
     world_camera_matrix = np.vstack([i_wc, j_wc, k_wc, last_row])
 
     return world_camera_matrix
 
+
 def get_camera_world_matrix(po, look_at, avup):
     ic, jc, kc = get_ijk(po, look_at, avup)
     
-    #Appending the last row [0 0 0 1] for the C->W Matrix
+    # Appending the last row [0 0 0 1] for the C->W Matrix
     i_cw = np.append(ic, 0)
     j_cw = np.append(jc, 0)
     k_cw = np.append(kc, 0)
     po_cw = np.append(po, 1)
 
-    #Stacking arrays as columns for the C->W Matrix
+    # Stacking arrays as columns for the C->W Matrix
     camera_world_matrix = np.column_stack((i_cw, j_cw, k_cw, po_cw))
 
     return camera_world_matrix
 
+
 def get_ijk(po, look_at, avup):
-    #Calculating ic, jc, and kc
+    # Calculating ic, jc, and kc
     k = po - look_at
     kc = (k / np.linalg.norm(k))
     vup = avup - po
@@ -39,11 +42,15 @@ def get_ijk(po, look_at, avup):
 
     return ic, jc, kc
 
-if __name__ == '__main__':
-    po = np.array([1,1,2])
-    look_at = np.array([6,8,0])
-    avup = np.array([4,5,3])
+
+def main():
+    po = np.array([1, 1, 2])
+    look_at = np.array([6, 8, 0])
+    avup = np.array([4, 5, 3])
     wc = get_world_camera_matrix(po, look_at, avup)
     cw = get_camera_world_matrix(po, look_at, avup)
     print(cw, "\n\n", wc)
-    
+
+
+if __name__ == '__main__':
+   main()
