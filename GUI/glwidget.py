956349d1381b8e8fe2617ba1cc2f14ce7347061b
Esdras Lemos <esdras.lemos07@gmail.com>
2017-12-10 23:48:10 -0300

    Adding Part 2 - OpenGL, but still with some problems

--- GUI/glwidget.py
+++ GUI/glwidget.py
@@ -1,215 +1,87 @@
-import math
-
 from OpenGL.GL import *
-# from OpenGL.GLU import *
-from PyQt5.QtGui import QColor
+from OpenGL.GLU import *
 from PyQt5.QtWidgets import QOpenGLWidget
-from PyQt5.QtCore import (pyqtSignal,
-                        QPoint,
-                        QSize,
-                        Qt)
+from objectModeling import obj
+#from scenario.scenario import
 
 class GLWidget(QOpenGLWidget):
-    xRotationChanged = pyqtSignal(int)
-    yRotationChanged = pyqtSignal(int)
-    zRotationChanged = pyqtSignal(int)
-
     def __init__(self, parent=None):
         super(GLWidget, self).__init__(parent)
 
-        self.object = 0
-        self.xRot = 0
-        self.yRot = 0
-        self.zRot = 0
-        
-        self.a = [0,0,0]
-        self.b = [1,0,0]
-        self.c = [0.5, 0, 0.8660254037844386]
-        self.d = [0.5, 0.8164965809277259, 0.29]
-
-        self.lastPos = QPoint()
-
-        self.trolltechGreen = QColor.fromCmykF(0.40, 0.0, 1.0, 0.0)
-        self.trolltechPurple = QColor.fromCmykF(0.39, 0.39, 0.0, 0.0)
-
-    def minimumSizeHint(self):
-        return QSize(50, 50)
-
-    def sizeHint(self):
-        return QSize(400, 400)
-
-    def setXRotation(self, angle):
-        angle = self.normalizeAngle(angle)
-        if angle != self.xRot:
-            self.xRot = angle
-            self.xRotationChanged.emit(angle)
-            self.update()
-
-    def setYRotation(self, angle):
-        angle = self.normalizeAngle(angle)
-        if angle != self.yRot:
-            self.yRot = angle
-            self.yRotationChanged.emit(angle)
-            self.update()
-
-    def setZRotation(self, angle):
-        angle = self.normalizeAngle(angle)
-        if angle != self.zRot:
-            self.zRot = angle
-            self.zRotationChanged.emit(angle)
-            self.update()
 
     def initializeGL(self):
-        self.setClearColor(self.trolltechPurple.darker())
-        self.object = self.makeObject()
-        glShadeModel(GL_FLAT)
+        glClearColor(0,0,0,0)
         glEnable(GL_DEPTH_TEST)
-        glEnable(GL_CULL_FACE)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
 
-    def paintGL(self):
-        glClear(
-                GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()
-        glTranslated(0.0, 0.0, -10.0)
-        glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)
-        glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)
-        glRotated(self.zRot / 16.0, 0.0, 0.0, 1.0)
-        glCallList(self.object)
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [1., 1., 1.])
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, [1., 1., 1.])
+        glLightfv(GL_LIGHT0, GL_SPECULAR, [1., 1., 1.])
+        glLightfv(GL_LIGHT0, GL_POSITION, [1.5, 2.9, 2.])
+
+        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, [1., 1., 1.])
+        glEnable(GL_CULL_FACE)
+        glFrontFace(GL_CCW)
+
 
     def resizeGL(self, width, height):
-        side = min(width, height)
-        if side < 0:
-            return
-
-        glViewport((width - side) // 2, (height - side) // 2, side,
-                side)
-
+        glViewport(0, 0, width, height)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        glOrtho(-0.5, +0.5, +0.5, -0.5, 4.0, 15.0)
+        gluPerspective(30, width / height, 0.5, 10)
+        gluLookAt(1.5,1.5,9, 1.5,1.5,0, 0,1,0)
+
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
 
-    def mousePressEvent(self, event):
-        self.lastPos = event.pos()
+        rgb_wall_material = [44/255, 137/255, 142/255]
+        wall_material = obj.Material(rgb_wall_material, rgb_wall_material, rgb_wall_material, 1)
+        cube = obj.Obj().import_obj('../objects/cube.obj', wall_material)
 
-    def mouseMoveEvent(self, event):
-        dx = event.x() - self.lastPos.x()
-        dy = event.y() - self.lastPos.y()
+        # CHÃO
+        glPushMatrix()
+        glScalef(3., 0.1, 4.)
+        self.draw_polygon(cube)
+        glPopMatrix()
 
-        if event.buttons() & Qt.LeftButton:
-            self.setXRotation(self.xRot + 8 * dy)
-            self.setYRotation(self.yRot + 8 * dx)
-        elif event.buttons() & Qt.RightButton:
-            self.setXRotation(self.xRot + 8 * dy)
-            self.setZRotation(self.zRot + 8 * dx)
+        # PAREDE ESQUERDA
+        glPushMatrix()
+        glScalef(0.1, 3., 0.75)
+        self.draw_polygon(cube)
+        glPopMatrix()
 
-        self.lastPos = event.pos()
+        glPushMatrix()
+        glTranslatef(0, 0, 1.75)
+        glScalef(0.1, 3., 0.5)
+        self.draw_polygon(cube)
+        glPopMatrix()
 
-    def makeObject(self):
-        genList = glGenLists(1)
-        glNewList(genList, GL_COMPILE)
+        glPushMatrix()
+        glScalef(0.1, 1., 4.)
+        self.draw_polygon(cube)
+        glPopMatrix()
 
-       	glBegin(GL_TRIANGLES)
-       	glVertex3f(self.a[0],self.a[1],self.a[2])
-       	glVertex3f(self.c[0],self.c[1],self.c[2])
-       	glVertex3f(self.d[0],self.d[1],self.d[2])
-       	glEnd()
-        #
-       	glBegin(GL_TRIANGLES)
-       	glVertex3f(self.a[0],self.a[1],self.a[2])
-       	glVertex3f(self.b[0],self.b[1],self.b[2])
-       	glVertex3f(self.d[0],self.d[1],self.d[2])
-       	glEnd()
-        #
-       	glBegin(GL_TRIANGLES)
-       	glVertex3f(self.a[0],self.a[1],self.a[2])
-       	glVertex3f(self.c[0],self.c[1],self.c[2])
-       	glVertex3f(self.b[0],self.b[1],self.b[2])
-       	glEnd()
-        #
-       	glBegin(GL_TRIANGLES)
-       	glVertex3f(self.c[0],self.c[1],self.c[2])
-       	glVertex3f(self.b[0],self.b[1],self.b[2])
-       	glVertex3f(self.d[0],self.d[1],self.d[2])
-       	glEnd()
-        # x1 = +0.06
-        # y1 = -0.14
-        # x2 = +0.14
-        # y2 = -0.06
-        # x3 = +0.08
-        # y3 = +0.00
-        # x4 = +0.30
-        # y4 = +0.22
-
-        # self.quad(x1, y1, x2, y2, y2, x2, y1, x1)
-        # self.quad(x3, y3, x4, y4, y4, x4, y3, x3)
-
-        # self.extrude(x1, y1, x2, y2)
-        # self.extrude(x2, y2, y2, x2)
-        # self.extrude(y2, x2, y1, x1)
-        # self.extrude(y1, x1, x1, y1)
-        # self.extrude(x3, y3, x4, y4)
-        # self.extrude(x4, y4, y4, x4)
-        # self.extrude(y4, x4, y3, x3)
-
-        # NumSectors = 200
-
-        # for i in range(NumSectors):
-        #     angle1 = (i * 2 * math.pi) / NumSectors
-        #     x5 = 0.30 * math.sin(angle1)
-        #     y5 = 0.30 * math.cos(angle1)
-        #     x6 = 0.20 * math.sin(angle1)
-        #     y6 = 0.20 * math.cos(angle1)
-
-        #     angle2 = ((i + 1) * 2 * math.pi) / NumSectors
-        #     x7 = 0.20 * math.sin(angle2)
-        #     y7 = 0.20 * math.cos(angle2)
-        #     x8 = 0.30 * math.sin(angle2)
-        #     y8 = 0.30 * math.cos(angle2)
-
-        #     self.quad(x5, y5, x6, y6, x7, y7, x8, y8)
-
-        #     self.extrude(x6, y6, x7, y7)
-        #     self.extrude(x8, y8, x5, y5)
-
-        # glEnd()
-        glEndList()
-
-        return genList
-
-    def quad(self, x1, y1, x2, y2, x3, y3, x4, y4):
-        self.setColor(self.trolltechGreen)
-
-        glVertex3d(x1, y1, -0.05)
-        glVertex3d(x2, y2, -0.05)
-        glVertex3d(x3, y3, -0.05)
-        glVertex3d(x4, y4, -0.05)
-
-        glVertex3d(x4, y4, +0.05)
-        glVertex3d(x3, y3, +0.05)
-        glVertex3d(x2, y2, +0.05)
-        glVertex3d(x1, y1, +0.05)
-
-    def extrude(self, x1, y1, x2, y2):
-        self.setColor(self.trolltechGreen.darker(250 + int(100 * x1)))
-
-        glVertex3d(x1, y1, +0.05)
-        glVertex3d(x2, y2, +0.05)
-        glVertex3d(x2, y2, -0.05)
-        glVertex3d(x1, y1, -0.05)
-
-    def normalizeAngle(self, angle):
-        while angle < 0:
-            angle += 360 * 16
-        while angle > 360 * 16:
-            angle -= 360 * 16
-        return angle
-
-    def setClearColor(self, c):
-        glClearColor(c.redF(), c.greenF(), c.blueF(), c.alphaF())
-
-    def setColor(self, c):
-        glColor4f(c.redF(), c.greenF(), c.blueF(), c.alphaF())
+        # PAREDE DIREITA
+        glPushMatrix()
+        glTranslatef(2.9, 0., 0.)
+        glScalef(0.1, 3., 4.)
+        self.draw_polygon(cube)
+        glPopMatrix()
 
 
+    def draw_polygon(self, obj):
+        for face in obj.faces:
+            glMaterialfv(GL_FRONT, GL_AMBIENT, face.material.k_a_rgb)
+            glMaterialfv(GL_FRONT, GL_DIFFUSE, face.material.k_d_rgb)
+            glMaterialfv(GL_FRONT, GL_SPECULAR, face.material.k_e_rgb)
+            glMaterialf(GL_FRONT, GL_SHININESS, face.material.attenuation)
 
+            glBegin(GL_POLYGON)
+            for vertex in face.vertices:
+                #glNormal3f(face.normal[0], face.normal[1], face.normal[2])
+                glVertex3f(vertex.coordinates[0], vertex.coordinates[1], vertex.coordinates[2])
+            glEnd()
--- GUI/mainwindow.py
+++ GUI/mainwindow.py
@@ -1,102 +1,13 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QImage,QPixmap
-from PyQt5.QtWidgets import (QHBoxLayout,
-							QVBoxLayout,
-							QSlider,
-        					QWidget,
-        					QLabel,
-        					QTabWidget)
+from PyQt5.QtWidgets import (QVBoxLayout, QWidget)
 
 from glwidget import GLWidget
 
 class Window(QWidget):
-    def __init__(self,width,height):
+    def __init__(self, width, height):
         super(Window, self).__init__()
-        self.setFixedSize(width,height);
+        self.setFixedSize(width, height)
 
-        self.gl_tab = QWidget()
-        self.ray_casting_tab = QWidget()
-        self.tablewidget = QTabWidget()
-        self.mainLayout = QVBoxLayout(self)
-        
-        ############################################
-        # Ray Casting
-        # 
-        rayCastingLayout = QVBoxLayout()
-        self.imageLabel = QLabel()
-        
-        rayCastingLayout.addWidget(self.imageLabel)
-        self.ray_casting_tab.setLayout(rayCastingLayout)
-        
-        ############################################
-        # OpenGL
         self.glWidget = GLWidget()
-
-        self.xSlider = self.createSlider()
-        self.ySlider = self.createSlider()
-        self.zSlider = self.createSlider()
-
-        self.xSlider.valueChanged.connect(self.glWidget.setXRotation)
-        self.glWidget.xRotationChanged.connect(self.xSlider.setValue)
-        self.ySlider.valueChanged.connect(self.glWidget.setYRotation)
-        self.glWidget.yRotationChanged.connect(self.ySlider.setValue)
-        self.zSlider.valueChanged.connect(self.glWidget.setZRotation)
-        self.glWidget.zRotationChanged.connect(self.zSlider.setValue)
-
-        self.horizontalx = QHBoxLayout()
-        self.horizontaly = QHBoxLayout()
-        self.horizontalz = QHBoxLayout()
-
-        self.labelx = QLabel("X axis")
-        self.labely = QLabel("Y axis")
-        self.labelz = QLabel("Z axis")
-
-        self.horizontalx.addWidget(self.labelx)
-        self.horizontalx.addWidget(self.xSlider)
-        self.horizontaly.addWidget(self.labely)
-        self.horizontaly.addWidget(self.ySlider)
-        self.horizontalz.addWidget(self.labelz)
-        self.horizontalz.addWidget(self.zSlider)
-
-        glLayout = QVBoxLayout()
-        glLayout.addWidget(self.glWidget)
-        glLayout.addLayout(self.horizontalx)
-        glLayout.addLayout(self.horizontaly)
-        glLayout.addLayout(self.horizontalz)
-        self.gl_tab.setLayout(glLayout)
-
-        self.xSlider.setValue(15 * 16)
-        self.ySlider.setValue(345 * 16)
-        self.zSlider.setValue(0 * 16)
-        ############################################
-
-        self.tablewidget.addTab(self.ray_casting_tab,"RayCasting")
-        self.tablewidget.addTab(self.gl_tab,"OpenGL")
-
-        self.mainLayout.addWidget(self.tablewidget)
-        self.setLayout(self.mainLayout)
-        self.setWindowTitle("Computer Graphics Project")
-
-    def createSlider(self):
-        slider = QSlider(Qt.Horizontal)
-
-        slider.setRange(0, 360 * 16)
-        slider.setSingleStep(16)
-        slider.setPageStep(15 * 16)
-        slider.setTickInterval(15 * 16)
-        slider.setTickPosition(QSlider.TicksRight)
-
-        return slider
-
-    def computeOpeningSize(self):
-        width = self.ray_casting_tab.frameGeometry().width()
-        height = self.ray_casting_tab.frameGeometry().height()
-        return width,height
-
-
-    def paint(self, screen):
-        image = QImage(screen, screen.shape[1],\
-                            screen.shape[0],QImage.Format_RGB888)
-        
-        pix = QPixmap.fromImage(image)
-        self.imageLabel.setPixmap(pix)
+        mainLayout = QVBoxLayout(self)
+        mainLayout.addWidget(self.glWidget)
+        self.setLayout(mainLayout)
